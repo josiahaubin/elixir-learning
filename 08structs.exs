6f2615eae5a87dce16145fa2211a5fe18c02f228
josiahaubin <josiahaubin@gmail.com>
2022-03-02 13:56:05 -0700

    more learning

---
+++ 08structs.exs
@@ -0,0 +1,28 @@
+defmodule User do
+  defstruct name: "John", age: 29
+end
+
+defmodule Test do
+  require User
+
+  def main do
+    john = %User{}
+    IO.inspect(john)
+
+    IO.puts("Another one")
+
+    jane = %User{name: "Jane"}
+    IO.inspect(jane)
+    jane = %{jane | age: 31}
+    IO.inspect(jane)
+
+    IO.puts("Jane's age is #{jane.age}")
+
+    # IO.puts("Occupation")
+    # Can't do this, everything must be defined in the struct
+    # jane = %{jane | occ: "Lawyer"}
+    # IO.inspect(jane)
+  end
+end
+
+Test.main()
